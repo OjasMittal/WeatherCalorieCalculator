b99a149370e63836ff99779768c5f2ab5eaa32bb
Ojas Mittal <ojasfarm31@gmail.com>
2022-10-29 15:41:36 +0530

    improved UI with sliders

--- calorie.py
+++ calorie.py
@@ -11,14 +11,14 @@ class Calorie:
 
     def calculate(self):
         if self.type==1:
-            result = int(10 * self.weight + 6.5 * self.height + 5 - self.temperature * 10)
+            result = int(22 * self.weight + 6.5 * self.height + 5 - self.temperature * 10)
             return result
         elif self.type==2:
-            result = int(10 * self.weight + 6.5 * self.height + 5 - (self.temperature-32) * 50/9)
+            result = int(22 * self.weight + 6.5 * self.height + 5 - (self.temperature-32) * 50/9)
             return result
 
 
 if __name__ == "__main__":
     temperature = Temperature(country="india", city="mumbai").get()
-    calorie = Calorie(weight=115, height=185.4, age=19, temperature=temperature)
+    calorie = Calorie(weight=95, height=185, age=20, temperature=temperature)
     print(calorie.calculate())
